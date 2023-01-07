133cf9e6d24feaf278b601f00b9a3d89f6cf99ef
Yankun Song <yankun.l.song@gmail.com>
2023-01-07 11:34:11 -0500

    1

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # cdk-backend
 
 ## set up the node boilerplate
+
 npm init -y  
 npm i -D aws-cdk aws-cdk-lib constructs ts-node typescript
 npm install -D @types/node
 
 ## create infra folder
+
 create launcher.ts and spaceStack.ts
 
 ## create the ts config
+
 tsc --init
 use the given config to avoid potential errors
+
+## add contents to stacks
+
+## install lambda nodejs to compile .ts files for lambda functions
+
+https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs-readme.html#local-bundling
+npm install --save-dev esbuild@0
--- infra/SpaceStack.ts
+++ infra/SpaceStack.ts
@@ -8,6 +8,7 @@ import {
 import { join } from "path";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { GenericTable } from "./GenericTable";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "spaceApi");
@@ -16,15 +17,25 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // create a lambda function with a .js file
     const helloLambda = new LambdaFunction(this, "helloLambda", {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.main",
     });
 
-    // hello api lambda integration
+    // create a lambda function with a .ts file
+    const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
+      entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
+      handler: "handler",
+    });
+
+    // integrate lambda with api gateway
+    // 1. create a lambda integration
     const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    // 2. create a api gateway resource
     const helloLambdaResource = this.api.root.addResource("hellos");
+    // 3. attach the integration to the resource
     helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
 }
